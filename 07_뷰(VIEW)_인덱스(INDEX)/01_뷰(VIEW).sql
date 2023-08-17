fdb4bbc0758dc6db153c629ed6537c7c8dc5a200
Lee Ju Nyung <139120379+JunyungLee@users.noreply.github.com>
2023-08-17 18:23:40 +0900

    Add files via upload

--- 07_뷰(VIEW)_인덱스(INDEX)/01_뷰(VIEW).sql
+++ 07_뷰(VIEW)_인덱스(INDEX)/01_뷰(VIEW).sql
@@ -13,52 +13,45 @@ SELECT 문장
 DROP VIEW 뷰이름;
 ***************************/
 SELECT * FROM BOOK WHERE BOOKNAME LIKE '%축구%';
--- 뷰(VIEW) 만들기 
-CREATE VIEW VW_BOOK 
-AS 
-SELECT * FROM BOOK WHERE BOOKNAME LIKE '%축구%'
-;
-
-CREATE OR REPLACE VIEW VW_BOOK  
-AS 
+-- 뷰(VIEW) 만들기
+CREATE OR REPLACE VIEW VW_BOOK
+AS
 SELECT * FROM BOOK WHERE BOOKNAME LIKE '%축구%'
 WITH READ ONLY
 ;
---뷰 사용해서 데이터 검색
+-- 뷰 사용해서 데이터 검색
 SELECT * FROM VW_BOOK;
 SELECT * FROM (SELECT * FROM BOOK WHERE BOOKNAME LIKE '%축구%');
 
-SELECT * FROM (SELECT * FROM BOOK WHERE BOOKNAME LIKE '%축구%') WHERE PUBLISHER = '굿스포츠';
-SELECT * FROM VW_BOOK WHERE PUBLISHER = '굿스포츠';
----------------
-
+SELECT * FROM VW_BOOK
+WHERE PUBLISHER = '굿스포츠';
+---------------------
 CREATE OR REPLACE VIEW VW_BOOK2
-    (B_ID, B_NAME, B_PB, B_PR)
+    (BID, BNAME, PUB, PRICE) -- 컬럼 별칭 사용
 AS
-SELECT BOOKID, BOOKNAME, PUBLISHER, PRICE FROM BOOK WHERE BOOKNAME LIKE '%축구%'
+SELECT BOOKID, BOOKNAME, PUBLISHER, PRICE
+FROM BOOK
+WHERE BOOKNAME LIKE '%축구%'
 WITH READ ONLY
 ;
---뷰 삭제 
-DROP VIEW VW_BOOK; --뷰 삭제 처리 완료 
-------------------------
---VIEW = 가상테이블
+----------
+-- 뷰 삭제
+DROP VIEW VW_BOOK; --뷰 삭제
+----------------------------
 CREATE OR REPLACE VIEW VW_ORDERS
 AS
-SELECT O.*, B.BOOKNAME, B.PUBLISHER, B.PRICE, C.NAME, C.ADDRESS, C.PHONE
+SELECT O.*,
+       B.BOOKNAME, B.PUBLISHER, B.PRICE,
+       C.NAME, C.ADDRESS, C.PHONE
 FROM ORDERS O, BOOK B, CUSTOMER C
-WHERE O.BOOKID = B.BOOKID 
+WHERE O.BOOKID = B.BOOKID
 AND O.CUSTID = C.CUSTID
 WITH READ ONLY
 ;
 SELECT * FROM VW_ORDERS WHERE NAME = '추신수';
 SELECT * FROM VW_ORDERS WHERE PUBLISHER = '굿스포츠';
---위와 동일 => INLINE VIEW (FROM-SUB QUERY)
-SELECT * FROM(SELECT O.*, B.BOOKNAME, B.PUBLISHER, B.PRICE, C.NAME, C.ADDRESS, C.PHONE
-FROM ORDERS O, BOOK B, CUSTOMER C
-WHERE O.BOOKID = B.BOOKID 
-AND O.CUSTID = C.CUSTID)
-WHERE PUBLISHER = '굿스포츠';
---===================================
+
+--===============================
 --(실습) 뷰를 생성, 조회, 삭제
 --1. 뷰생성 - 뷰명칭 : VW_ORD_ALL
 ---- 주문(판매)정보, 책정보, 고객정보 모두 조회할 수 있는 형태 뷰 
@@ -68,25 +61,21 @@ WHERE PUBLISHER = '굿스포츠';
 ---- 정렬 : 구입한 사람이름, 구입일자 최근순
 --4. 판매된 책에 대하여 구매자별 건수, 합계금액, 평균금액, 최고가, 최저가 조회
 --(최종) 뷰삭제 : VW_ORD_ALL 삭제 처리
-------------------------------------------
---1. 뷰생성 - 뷰명칭 : VW_ORD_ALL
----- 주문(판매)정보, 책정보, 고객정보 모두 조회할 수 있는 형태 뷰 
-CREATE OR REPLACE VIEW VW_ORD_ALL
-AS 
-SELECT O.*, B.BOOKNAME, B.PUBLISHER, B.PRICE, C.NAME, C.ADDRESS, C.PHONE
+--=============================
+CREATE OR REPLACE VIEW VW_ORD_ALL 
+AS
+SELECT O.ORDERID, O.CUSTID, C.NAME,
+       O.BOOKID, B.BOOKNAME,
+       B.PRICE, O.SALEPRICE, O.ORDERDATE,
+       B.PUBLISHER, C.ADDRESS, C.PHONE
 FROM ORDERS O, BOOK B, CUSTOMER C
-WHERE O.BOOKID = B.BOOKID 
-AND O.CUSTID = C.CUSTID
+WHERE O.BOOKID = B.BOOKID --조인조건
+AND O.CUSTID = C.CUSTID --조인조건
+
 WITH READ ONLY
 ;
---2. 이상미디어에서 출판한 책중 판매된 책제목, 판매금액, 판매일 조회
-SELECT BOOKNAME, SALEPRICE, ORDERDATE FROM VW_ORD_ALL WHERE PUBLISHER = '이상미디어';
---3. 이상미디어에서 출판한 책 중에서 추신수, 장미란이 구매한 책 정보 조회
----- 출력항목 : 구입한 사람이름, 책제목, 출판사, 원가(정가), 판매가, 판매일자
----- 정렬 : 구입한 사람이름, 구입일자 최근순
-SELECT NAME, BOOKNAME, PUBLISHER, PRICE, SALEPRICE, ORDERDATE FROM VW_ORD_ALL 
-WHERE PUBLISHER = '이상미디어' ORDER BY NAME, ORDERDATE;
---4. 판매된 책에 대하여 구매자별 건수, 합계금액, 평균금액, 최고가, 최저가 조회
-SELECT NAME, COUNT(*), SUM(PRICE), TRUNC(AVG(PRICE)), MAX(PRICE), MIN(PRICE) FROM VW_ORD_ALL GROUP BY NAME;
---(최종) 뷰삭제 : VW_ORD_ALL 삭제 처리
-DROP VIEW VW_ORD_ALL;
+SELECT * FROM VW_ORD_ALL WHERE NAME = '박지성';
+
+
+
+
